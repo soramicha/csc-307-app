95229433e09a9ae1fb39b213c82b17aed8d47d9d
soramicha <schang7jcu@gmail.com>
2025-01-13 14:40:06 -0800

    Step 4 completed of IE2

--- packages/express-backend/backend.js
+++ packages/express-backend/backend.js
@@ -18,3 +18,53 @@ console.log(
     `Example app listening at http://localhost:${port}`
 );
 });
+
+const users = {
+    users_list: [
+      {
+        id: "xyz789",
+        name: "Charlie",
+        job: "Janitor"
+      },
+      {
+        id: "abc123",
+        name: "Mac",
+        job: "Bouncer"
+      },
+      {
+        id: "ppp222",
+        name: "Mac",
+        job: "Professor"
+      },
+      {
+        id: "yat999",
+        name: "Dee",
+        job: "Aspring actress"
+      },
+      {
+        id: "zap555",
+        name: "Dennis",
+        job: "Bartender"
+      }
+    ]
+  }
+
+const findUserByName = (name) => {
+    // only return information where the user["name"] === name
+    return users["users_list"].filter(
+        (user) => user["name"] === name
+    );
+};
+
+app.get("/users", (req, res) => {
+    // get the requested name from the url
+    const name = req.query.name;
+    if (name != undefined) {
+        let result = findUserByName(name);
+        result = { users_list: result };
+        res.send(result);
+    } else {
+        res.send(users);
+    }
+});
+
